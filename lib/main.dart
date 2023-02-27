64dbcc36e50d79aec467b05e94af04052de3e5d6
Polotacki <nasserabdo98@gmail.com>
2023-02-27 15:00:27 +0200

    reformat code

--- lib/main.dart
+++ lib/main.dart
@@ -164,7 +164,7 @@ class MyApp extends StatelessWidget {
                             backgroundColor: light,
                           ),
                           bottomNavigationBarTheme:
-                          const BottomNavigationBarThemeData(
+                              const BottomNavigationBarThemeData(
                             backgroundColor: light,
                             type: BottomNavigationBarType.fixed,
                           ),
